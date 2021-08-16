4536b978fa82391d351bbe98cdfbd23ec675e3c6
Christina Nguyen <cnguyen4290@yahoo.com>
2021-08-15 22:14:06 -0500

    commit tree 3a75d040db8488ee704aa3f9f4c7b635ad440a3a

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+console.log(customerName);
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'jake'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Mary';
+}
